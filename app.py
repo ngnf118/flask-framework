4c1014dbffd814db98fb4dc08474a21ced2c10a0
unknown <ngnf118@gmail.com>
2017-12-28 15:40:44 -0800

    tired

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 import simplejson
 
-quandl.ApiConfig.api_key = "bbcN1HBxBVzay8-qxx7B"
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+quandl.ApiConfig.api_key = "bbcN1HBxBVzay8-qxx7B"
+
+
 # @app.route('/graph')
 # def graph():
 #     return render_template('graph.html')
+
 
 if __name__ == '__main__':
     app.run(port=33507)
