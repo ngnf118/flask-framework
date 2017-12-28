7ab828f6d09d9013de7d51c1fb40ef4132b8f479
unknown <ngnf118@gmail.com>
2017-12-28 15:48:04 -0800

    back

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 from flask import Flask, render_template, request, redirect
-import pandas as pd
-import requests
-import quandl
-from bokeh.plotting import figure
-from bokeh.embed import components
-import simplejson
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-# quandl.ApiConfig.api_key = "bbcN1HBxBVzay8-qxx7B"
-# data = quandl.get(" WIKI/PRICES")
-
-# @app.route('/graph')
-# def graph():
-#     return render_template('graph.html')
-
+  return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=33507)
-
+  app.run(port=33507)
---
+++ test_api_plot.py
@@ -0,0 +1,4 @@
+import quandl
+quandl.ApiConfig.api_key = "bbcN1HBxBVzay8-qxx7B"
+data = quandl.get_table(" WIKI/PRICES")
+
