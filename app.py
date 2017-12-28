bd558f181f1c6bbc4aa6a3620b389d957ff12607
unknown <ngnf118@gmail.com>
2017-12-28 15:33:50 -0800

    herewego

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 import requests
+import quandl
 from bokeh.plotting import figure
 from bokeh.embed import components
+import simplejson
+
+quandl.ApiConfig.api_key = "bbcN1HBxBVzay8-qxx7B"
+
 
 app = Flask(__name__)
 
@@ -12,17 +17,7 @@ def index():
 
 @app.route('/graph')
 def graph():
-    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
-    session = requests.Session()
-    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-    raw_data = session.get(api_url)
-    # my_df = pd.DataFrame(raw_data)
-    plot = figure(tools=TOOLS,
-                  title='Data from Quandle WIKI set',
-                  x_axis_label='date',
-                  x_axis_type='datetime')
-    script, div = components(plot)
-    return render_template('graph.html', script=script, div=div)
+    return render_template('graph.html')
 
 if __name__ == '__main__':
     app.run(port=33507)
