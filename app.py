72b92d4024e3bfa3497d222fa5f807baaae01600
unknown <ngnf118@gmail.com>
2017-12-28 14:40:47 -0800

    try

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request, redirect
 import pandas
 
+def stock_to_df(my_string):
+    return
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/graph')
-# def stock_to_df(my_string):
-#     something
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
 
 if __name__ == '__main__':
     app.run(port=33507)
