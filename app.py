9c73d6c2de6f069a2701b7a060dc66760cc97e8f
unknown <ngnf118@gmail.com>
2017-12-28 14:05:53 -0800

    hope

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/graph')
-def stock_to_df(my_string):
-    # something
+# @app.route('/graph')
+# def stock_to_df(my_string):
+#     something
 
 if __name__ == '__main__':
     app.run(port=33507)
