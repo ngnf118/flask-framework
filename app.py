c05e73439b08cf4ac71d50f7f5f2fef845ee0da8
unknown <ngnf118@gmail.com>
2017-12-28 17:08:42 -0800

    working

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def read_post():
-    text = request.form['ticker']
-    processed_text = text.upper()
-    api_plot.plot_it_out(processed_text)
-    return render_template('stockplot.html')
+# @app.route('/', methods=['POST'])
+# def read_post():
+#     text = request.form['ticker']
+#     processed_text = text.upper()
+#     api_plot.plot_it_out(processed_text)
+#     return render_template('stockplot.html')
 
 if __name__ == '__main__':
     app.run(port=33507)
