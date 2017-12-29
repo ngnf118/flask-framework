a3f0c7bb2703b7951b202af847ac23f4de681e6c
unknown <ngnf118@gmail.com>
2017-12-28 17:31:35 -0800

    demo

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import numpy as np
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, show, output_file
 
+app = Flask(__name__)
+
 def read_data(stock):
     quandl.ApiConfig.api_key = "bbcN1HBxBVzay8-qxx7B"
     data = quandl.get("WIKI/"+stock, start_date="2017-11-01", end_date="2017-11-30")
@@ -15,7 +17,7 @@ def datetime(x):
 
 def plot_it_out(stock):
     p1 = figure(x_axis_type="datetime", title="Stock Closing Prices")
-    p1.grid.grid_line_alpha=0.3
+    p1.grid.grid_line_alpha = 0.3
     p1.xaxis.axis_label = 'Date'
     p1.yaxis.axis_label = 'Price'
 
@@ -23,21 +25,20 @@ def plot_it_out(stock):
     p1.line(datetime(my_df.index), my_df['Close'], color='#000000', legend=stock)
     p1.legend.location = "top_left"
 
-    output_file("templates/stockplot.html", title="Graph of "+stock)
-    # show(gridplot([[p1]], plot_width=400, plot_height=400))
+    output_file("templates/stockplot.html", title="Graph of " + stock)
+    show(gridplot([[p1]], plot_width=400, plot_height=400))plot_it_out
 
 # plot_it_out('GOOG')
 # plot_it_out('AMZN')
 # plot_it_out('FB')
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def read_post():
+def read_in():
     text = request.form['ticker']
     processed_text = text.upper()
     plot_it_out(processed_text)
